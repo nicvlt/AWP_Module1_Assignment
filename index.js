2cd0028d1e04069610ff66b4fb6c55f164d59a8c
nicvlt <nicolas75d@gmail.com>
2022-10-01 19:00:07 -0700

    Change index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//change path to any filepath
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.sendFile('C:\\Users\\nicol\\Desktop\\Cours\\UCI\\Advanced Web Programming\\Week1\\AWP_Module1_Assignment\\img\\cat-bath.jpg')
+  res.send("A very empty project")
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
